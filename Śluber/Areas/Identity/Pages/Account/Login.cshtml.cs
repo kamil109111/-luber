380c18b2a95f4f7102dd7d2d1ea9572038aaf437
38444 <38444@studenci.pwsz.legnica.edu.pl>
2020-01-24 19:17:32 +0100

    tłumaczenie ekranu logowania na pl

--- Śluber/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Śluber/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace Śluber.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Zapamiętaj mnie")]
             public bool RememberMe { get; set; }
         }
 
